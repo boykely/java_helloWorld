eeb62f9c1a6e1cfd0ff9402c46ef1002e09485a4
boykely <valimo_ral@yahoo.fr>
2016-02-09 23:40:27 +0100

    + Migrate to opencv3.0
    flashTileCV and flashTile initalized

--- src/Form.java
+++ src/Form.java
@@ -20,7 +20,8 @@ import javax.swing.JScrollPane;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
+
+import org.opencv.imgcodecs.Imgcodecs;
 
 import javax.imageio.ImageIO;
 
@@ -30,9 +31,13 @@ public class Form extends JFrame
 	private ImageContainer _containerF;
 	private ImageContainer _containerG;
 	private ImageContainer _containerSVBRDF;
+	private ImageContainer _containerNext;
+	private ImageContainer _containerNextCV;
 	private JScrollPane _scrollF;
 	private JScrollPane _scrollG;
 	private JScrollPane _scrollSVBRDF;
+	private JScrollPane _scrollNext;
+	private JScrollPane _scrollNextCV;
 	private JPanel _containerToolBox;
 	private JButton _btnLoad;
 	private JButton _btnProcess;
@@ -55,6 +60,10 @@ public class Form extends JFrame
 	private Mat[][] guideTilesCV;
 	private Mat[] imageDataCV;
 	private Thread[] listThread;
+	//pour test image
+	private int next=0;
+	private int previous=0;
+	private int nnn=0;
 	public Form()
 	{		
 		this.setSize(1000, 900);
@@ -66,13 +75,19 @@ public class Form extends JFrame
 		_containerF=new ImageContainer();	
 		_containerG=new ImageContainer();
 		_containerSVBRDF=new ImageContainer();
+		_containerNext=new ImageContainer();
+		_containerNextCV=new ImageContainer();
 		_containerToolBox=new JPanel();
 		_scrollF=new JScrollPane(_containerF);
 		_scrollG=new JScrollPane(_containerG);
 		_scrollSVBRDF=new JScrollPane(_containerSVBRDF);
+		_scrollNext=new JScrollPane(_containerNext);
+		_scrollNextCV=new JScrollPane(_containerNextCV);
 		_scrollF.setPreferredSize(new Dimension(400,400));
 		_scrollG.setPreferredSize(new Dimension(400,400));
-		_scrollSVBRDF.setPreferredSize(new Dimension(1000, 400));
+		_scrollSVBRDF.setPreferredSize(new Dimension(197, 197));
+		_scrollNext.setPreferredSize(new Dimension(197, 197));
+		_scrollNextCV.setPreferredSize(new Dimension(197, 197));
 		
 		
 		_containerF.setBackground(Color.black);
@@ -88,7 +103,7 @@ public class Form extends JFrame
 			
 		
 		_btnLoad=new JButton("Charger Photos");		
-		_btnProcess=new JButton("Générer SVBRDF");
+		_btnProcess=new JButton("Défiler Tiles");
 		
 		
 		_containerToolBox.setLayout(_layoutToolBox);	
@@ -99,6 +114,8 @@ public class Form extends JFrame
 		this.add(_scrollG);
 		this.add(_containerToolBox);	
 		this.add(_scrollSVBRDF);
+		this.add(_scrollNext);
+		this.add(_scrollNextCV);
 		
 		imageData=new BufferedImage[2];
 		imageDataCV=new Mat[2];
@@ -173,7 +190,32 @@ public class Form extends JFrame
 				// TODO Auto-generated method stub
 				if(imageLoaded)
 				{
-					computeSVBRDF();
+					//computeSVBRDF();
+				}
+				//pour test images
+				
+				if(next<tileLenW)
+				{
+					//showCvDataToJava(flashTilesCV[nnn][next],_containerNext);
+					System.out.println(nnn+"-"+next);
+					showTile(flashTiles[nnn][next], _containerNext);					
+					showCvDataToJava(flashTilesCV[nnn][next], _containerNextCV);
+					next++;
+				}
+				else
+				{
+					nnn++;
+					if(nnn<tileLenH)
+					{
+						next=0;
+						//showCvDataToJava(flashTilesCV[nnn][next],_containerNext);
+						System.out.println(nnn+"-"+next);
+						showTile(flashTiles[nnn][next], _containerNext);						
+						showCvDataToJava(flashTilesCV[nnn][next], _containerNextCV);
+					}
+					else{
+						next=0;nnn=0;
+					}
 				}
 			}
 		});
@@ -190,8 +232,10 @@ public class Form extends JFrame
 			imageData[1]=ImageIO.read(imageFiles[1]);
 			System.out.println("Flash image:"+imageFiles[0].getAbsolutePath());
 			System.out.println("Guide image:"+imageFiles[1].getAbsolutePath());
-			imageDataCV[0]=Highgui.imread(imageFiles[0].getAbsolutePath());
-			imageDataCV[1]=Highgui.imread(imageFiles[1].getAbsolutePath());
+			imageDataCV[0]=Imgcodecs.imread(imageFiles[0].getAbsolutePath());
+			imageDataCV[1]=Imgcodecs.imread(imageFiles[1].getAbsolutePath());	
+			System.out.println(imageDataCV[0]);
+			System.out.println(imageDataCV[1]);
 			_containerF.image=imageData[0];
 			_containerG.image=imageData[1];
 			_containerF.setIcon(new ImageIcon(imageData[0]));
@@ -214,7 +258,7 @@ public class Form extends JFrame
 			System.out.println("Exception lancé par chargement d'images:");
 		}
 		catch(Exception e){
-			System.out.println(e.getMessage());
+			System.out.println("Select images:"+e.getMessage());
 		}
 	}
 	private void computeSVBRDF()
@@ -238,8 +282,8 @@ public class Form extends JFrame
 				//System.out.println(i+"-"+j);
 				flashTiles[i][j]=new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_3BYTE_BGR);
 				guideTiles[i][j]=new BufferedImage(tileSize,tileSize,BufferedImage.TYPE_3BYTE_BGR);
-				flashTilesCV[i][j]=new Mat(tileSize,tileSize,CvType.CV_8UC3);
-				guideTilesCV[i][j]=new Mat(tileSize,tileSize,CvType.CV_8UC3);
+				flashTilesCV[i][j]=new Mat(tileSize,tileSize,imageDataCV[0].type());
+				guideTilesCV[i][j]=new Mat(tileSize,tileSize,imageDataCV[0].type());
 				//ligne
 				for(int s=0;s<tileSize;s++)
 				{
@@ -251,14 +295,25 @@ public class Form extends JFrame
 						rgb_=imageData[1].getRGB(t+tileSize*j, s+tileSize*i);
 						flashTiles[i][j].setRGB(t, s, rgb);
 						guideTiles[i][j].setRGB(t, s, rgb_);
-						dd=imageDataCV[0].get(s, t);	
-						dd_=imageDataCV[1].get(s,t);
+						//ceci ne marche pas.je ne sais pas encore
+						/*dd=imageDataCV[0].get(t+tileSize*j, s+tileSize*i);	
+						dd_=imageDataCV[1].get(t+tileSize*j,s+tileSize*i);						
 						flashTilesCV[i][j].put(s, t, dd);
-						guideTilesCV[i][j].put(s, t, dd_);
+						guideTilesCV[i][j].put(s, t, dd_);*/
 					}					
 				}				
 			}
-			if(i==3)this.showCvDataToJava(flashTilesCV[i][4],_containerSVBRDF);
+			//if(i==4)this.showCvDataToJava(flashTilesCV[4][5],_containerSVBRDF);
+			if(i==4)this.showTile(flashTiles[4][5],_containerSVBRDF);
+		}
+		Mat temp;
+		for(int i=0;i<tileLenH;i++)
+		{
+			for(int j=0;j<tileLenW;j++)
+			{
+				temp=convertTileToCV(flashTiles[i][j]);
+				flashTilesCV[i][j]=temp;
+			}
 		}
 		System.out.println("Initialisation des tiles terminé");
 		listThread=new Thread[tileLenH*tileLenW];
@@ -297,5 +352,17 @@ public class Form extends JFrame
 		final byte[] containerPixels=((DataBufferByte)container.image.getRaster().getDataBuffer()).getData();
 		System.arraycopy(data, 0, containerPixels, 0, bufferSize);
 		container.setIcon(new ImageIcon(container.image));
+	}
+	private void showTile(BufferedImage im,ImageContainer container)
+	{
+		container.image=im;
+		container.setIcon(new ImageIcon(container.image));
+	}
+	private Mat convertTileToCV(BufferedImage im)
+	{
+		Mat m=new Mat(im.getHeight(), im.getWidth(), CvType.CV_8UC3);
+		byte[] data=((DataBufferByte)im.getRaster().getDataBuffer()).getData();
+		m.put(0, 0, data);
+		return m;
 	}
 }
